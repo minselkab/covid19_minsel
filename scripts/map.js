7dd855d12115ecbe3fc8fd75f4ecd0ab0903a804
diskominfo kabupaten minahasa selatan <63833890+minselkab@users.noreply.github.com>
2020-04-25 19:19:08 -0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,9 +128,9 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Kecamatan'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-           point['Positif'])+'<br>'+
-           point['Dp'])+'<br>'+
-           point['Sembuh'])+'<br>';
+          (point['Positif']+'<br>')+
+          (point['Dp']+'<br>')+
+          (point['Sembuh']+'<br>');
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
